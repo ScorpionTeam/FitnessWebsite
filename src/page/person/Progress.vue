dd55fd4e8f8f87cd573a46742e65b3f1b1e58c0d
HWS <15757135983@163.com>
2017-07-24 21:56:31 +0800

    1.进阶之路

--- src/page/person/Progress.vue
+++ src/page/person/Progress.vue
@@ -7,6 +7,7 @@
   <div>
     <Vheader></Vheader>
     <div class="content">
+      <Table title="进阶之路" :columns="columns()" :data="data" border ></Table>
     </div>
     <Vfooter></Vfooter>
   </div>
@@ -19,8 +20,69 @@
   export default{
     data(){
       return {
-        member: {}
+        member: {},
+        data:[
+          {
+            month:'一月份',
+            weight:10,
+            fat:10,
+            muscle:10,
+            all:1000
+          },
+          {
+            month:'二月份',
+            weight:20,
+            fat:10,
+            muscle:30,
+            all:1000
+          },
+          {
+            month:'三月份',
+            weight:20,
+            fat:10,
+            muscle:30,
+            all:1000
+          }
+        ]
       }
+    },
+    methods:{
+      columns(){
+        let column=[];
+        column.push({
+          title:'月份',
+          key:'month',
+          align:'center',
+          width:80
+        })
+        column.push({
+          title:'体重',
+          key:'weight',
+          align:'center',
+          width:70
+        })
+        column.push({
+          title:'体脂',
+          key:'fat',
+          align:'center',
+          width:70
+        })
+        column.push({
+          title:'肌肉',
+          key:'muscle',
+          align:'center',
+          width:70
+        })
+        column.push({
+          title:'综合',
+          key:'all',
+          align:'center',
+          width:85
+        })
+        return column
+      }
+    },
+    created:function () {
     },
     components:{
       'Vheader':Vheader,
