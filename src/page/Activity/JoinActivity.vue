50359dbb10ebcaecff57b3e60e8384af29a59d29
HWS <15757135983@163.com>
2017-08-07 22:20:35 +0800

    1.参加活动界面，增加参加活动人员的列表

--- src/page/Activity/JoinActivity.vue
+++ src/page/Activity/JoinActivity.vue
@@ -1,10 +1,45 @@
 <style scoped rel='stylesheet/scss' lang='scss'>
- .content{
-   h3{
-     margin: 2rem;
-     text-align: center;
-   }
- }
+  .content{
+    background: #fff;
+    padding-bottom: 2rem;
+    h3{
+      margin: 2rem;
+      text-align: center;
+    }
+    /*参加人员列表*/
+    .join-list{
+      padding-left: 2rem;
+      overflow: hidden;
+      background-color: #fff;
+      li{
+        list-style: none;
+        float: left;
+        width: 5rem;
+        height: 5rem;
+        margin: 2rem 2rem 0 0;
+        img{
+          width: 100%;
+          height: 100%;
+          vertical-align: top;
+          object-fit: cover;
+          border-radius: 50%;
+        }
+      }
+    }
+    /*报名按钮*/
+    .btn{
+      display: block;
+      width: 15rem;
+      height: 4rem;
+      margin: 2rem auto 0;
+      line-height: 4rem;
+      text-align: center;
+      color: #fff;
+      font-size: 1.6rem;
+      background-color: #00bfbf;
+      border-radius: .5rem;
+    }
+  }
 </style>
 <template>
   <div>
@@ -12,7 +47,12 @@
     <div class="content">
       <h3>{{ActContent.name}}</h3>
       <div class="detailContent"></div>
-      <Button @click="joinConfirm" type="primary" long> 参加</Button>
+      <div class="join-list">
+        <li v-for="(member,index) in memberList" :key="index">
+          <img :src="headerUrl+member.imgUrl" alt="">
+        </li>
+      </div>
+      <Button @click="joinConfirm" type="primary" class="btn"> 参加</Button>
     </div>
     <Vfooter></Vfooter>
   </div>
@@ -20,11 +60,14 @@
 <script>
   import Header from '../../components/Header.vue'
   import Footer from '../../components/Footer.vue'
+  import {headerUrl} from '../../common/WebApi'
   export default{
     data(){
       return{
         name:'',
-        ActContent:{}
+        ActContent:{},
+        headerUrl:'',
+        memberList:[]
       }
     },
     methods:{
@@ -32,33 +75,37 @@
       getContent(){
         let self = this;
         self.$http.get('/activity/activityInfo?id='+this.$route.params.id).then(function (res) {
-          self.ActContent =res.data
-          document.querySelector('.detailContent').innerHTML = res.data.contentHtml
+          if(res.result==1){
+            self.ActContent =res.data
+            self.memberList = res.data.memberList
+            document.querySelector('.detailContent').innerHTML = res.data.contentHtml
+          }
         })
       },
       joinConfirm(){
-          let self =this
-          this.$Modal.confirm({
-              title:'参加确认',
-              content:"确认参加活动么",
-              onOk:function () {
-                self.join()
-              }
-          })
+        let self =this
+        this.$Modal.confirm({
+          title:'参加确认',
+          content:"确认参加活动么",
+          onOk:function () {
+            self.join()
+          }
+        })
       },
       join(){
-          let self =this;
-           self.$http.post('/activity/apply?memberId='+localStorage.getItem('fitId')+'&activityId='+self.$route.params.id).then(function (res) {
-              console.log(res)
-             if(res.result==1){
-                  self.$Message.success('成功参加')
-             }else {
-                 self.$Message.error(res.error.message)
-             }
-           })
+        let self =this;
+        self.$http.post('/activity/apply?memberId='+localStorage.getItem('fitId')+'&activityId='+self.$route.params.id).then(function (res) {
+          console.log(res)
+          if(res.result==1){
+            self.$Message.success('成功参加')
+          }else {
+            self.$Message.error(res.error.message)
+          }
+        })
       }
     },
     created:function () {
+      this.headerUrl = headerUrl
       this.getContent()
     },
     components:{
